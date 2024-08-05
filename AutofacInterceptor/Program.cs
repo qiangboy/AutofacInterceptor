4fc71b85e62331184096595e81892640b3bc3b2b
qdf <792772007@qq.com>
2024-08-05 12:04:58 +0800

    删除注释

--- AutofacInterceptor/Program.cs
+++ AutofacInterceptor/Program.cs
@@ -16,16 +16,12 @@ builder.Host
         cb.RegisterType<UserService>().As<IUserService>().EnableInterfaceInterceptors().InterceptedBy(typeof(LoggingInterceptor)).InterceptedBy(typeof(TestTimingInterceptor));
     });
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
